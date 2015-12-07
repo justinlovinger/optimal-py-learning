986f3941d9289576fb44dd06a04522efbe1df761
Justin Lovinger <git@justinlovinger.com>
2015-12-07 11:16:54 -0500

    Some unit tests

---
+++ pynn/tests/test_network.py
@@ -0,0 +1,21 @@
+import pytest
+
+from pynn import network
+
+def test_network_validation_layers():
+    with pytest.raises(ValueError):
+        n = network.Network([None])
+
+    with pytest.raises(ValueError):
+        n = network.Network([1])
+
+    with pytest.raises(ValueError):
+        n = network.Network(['q'])
+
+    n = network.Network([network.Layer()])
+
+def test_network_validation_required_next():
+    with pytest.raises(ValueError):
+        n = network.Network([network.GrowingLayer(), network.Layer()])
+
+    n = network.Network([network.GrowingLayer(), network.SupportsGrowingLayer()])
