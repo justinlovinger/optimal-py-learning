bc1ff848afeb6bbae72716ba2fc731e12ba0cbca
Justin Lovinger <git@justinlovinger.com>
2017-04-30 03:31:20 -0400

    Update __init__.py to easily import models

---
+++ pynn/__init__.py
@@ -0,0 +1,8 @@
+# Add models
+from pynn.architecture.mlp import MLP
+from pynn.architecture.rbf import RBF
+from pynn.architecture.pbnn import PBNN
+from pynn.architecture.som import SOM
+
+# Add model building
+from pynn.network import Model
